7e197c6ac4f9a70c6ccb4ba5f7df15e5fd6bd5c0
Anik Alamgir <119867049+anik-alamgir@users.noreply.github.com>
2023-11-21 16:39:00 +0600

    Add files via upload

---
+++ queue.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+class queue{
+
+int*arr;
+int front;
+int back;
+
+public:
+    queue(int n)
+    {
+    arr=new int [n];
+    front=-1;
+    back=-1;
+    }
+
+};
+
+int main()
+
+{
+int queueSize=10;
+queue myQueqe(queueSize);
+return 0;
+
+}
